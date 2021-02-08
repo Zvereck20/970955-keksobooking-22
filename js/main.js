dfe26d2f70c49306a1a25a3f59f826143c379b6b
Zvereck20 <kerusov20@gmail.com>
2021-02-09 00:11:14 +1000

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,95 +1,82 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 // Функция возвращает целое число
 
 let returnRandomnInteger = (minNumber, maxNumber) => {
-  if (minNumber >= maxNumber) {
-    return 'Не верно введенные числа'
-  }
   return Math.round(Math.random() * (maxNumber - minNumber) + minNumber);
 };
-
 
 // Функция, возвращающая случайное число с плавающей точкой
 
 let returnRandomFractional = (minNumber, maxNumber, anotherNumber = 1) => {
-  if (minNumber >= maxNumber) {
-    return 'Не верно введенные числа'
-  }
   let randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
   return randomNumber.toFixed(anotherNumber);
 };
 
+const TYPES_RANDOM = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
 
-// Authur object
+const CHECK_IN_OUT_TYMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 
-let authorObject = {
-  avatar: () => {
-    let randomAdress = '0' + Math.round(Math.random() * (8 - 1) + 1);
-    return 'img/avatars/user' + randomAdress + '.png';
-  },
+const FEATURES_ARRAY = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS_ARRAY = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+let returnRandomArrayElement = (elements) => {
+  return elements[_.random(0, elements.length - 1)];
 };
 
-// Offer object
-
-let offerObject = {
-  title: 'Предложения',
-  address: '{{location.x}, {location.y}}',
-  price: 2800,
-  type: 'bungallow',
-  rooms: 2,
-  quests: 4,
-  checkin: '12:00',
-  checkout: '13:00',
-  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  description: 'Вид на море',
-  photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-};
-
-// Location object
-
-let locationObject = {
-  randomLatitude: () => {
-    let latitudeNumber = Math.random() * (35.70000 - 35.65000) + 35.65000;
-    return latitudeNumber.toFixed(5);
-  },
-  randomLongitude: () => {
-    let longitubeNumber = Math.random() * (139.80000 - 139.70000) + 139.70000;
-    return longitubeNumber.toFixed(5);
-  },
-};
-
-//  Generated object
-
-/*
-let generatedObject = {
-  authorObject: {
-    avatar: () => {
-      let randomAdress = '0' + Math.round(Math.random() * (8 - 1) + 1);
-      return 'img/avatars/user' + randomAdress + '.png';
+const GET_OBJECT = () => {
+  return {
+    author: {
+      avatar: `img/avatars/user/0${returnRandomnInteger(1, 8)}.png`,
     },
-  },
-  offerObject: {
-    title: 'Предложения',
-    address: '{{location.x}, {location.y}}',
-    price: 2800,
-    type: 'bungallow',
-    rooms: 2,
-    quests: 4,
-    checkin: '12:00',
-    checkout: '13:00',
-    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    description: 'Вид на море',
-    photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-  },
-  locationObject: {
-    randomLatitude: () => {
-      let latitudeNumber = Math.random() * (35.70000 - 35.65000) + 35.65000;
-      return latitudeNumber.toFixed(5);
+    offer: {
+      title: 'Лучший выбор',
+      address: `x: ${returnRandomFractional(35.65000, 35.70000, 5)} y: ${returnRandomFractional(139.70000, 139.80000, 5)}`,
+      price: returnRandomnInteger(1, 100),
+      type: returnRandomArrayElement(TYPES_RANDOM),
+      rooms: returnRandomnInteger(1, 5),
+      quests: returnRandomnInteger(1, 10),
+      checkin: returnRandomArrayElement(CHECK_IN_OUT_TYMES),
+      checkout: returnRandomArrayElement(CHECK_IN_OUT_TYMES),
+      features: FEATURES_ARRAY.slice(returnRandomnInteger(0, 5)),
+      description: 'Чистота, уют и комфорт',
+      photos: PHOTOS_ARRAY.slice(returnRandomnInteger(0, 2)),
     },
-    randomLongitude: () => {
-      let longitubeNumber = Math.random() * (139.80000 - 139.70000) + 139.70000;
-      return longitubeNumber.toFixed(5);
+    location: {
+      x: returnRandomFractional(35.65000, 35.70000, 5),
+      y: returnRandomFractional(139.70000, 139.80000, 5),
     },
-  },
+  };
 };
-*/
+
+const CREATE_ARRAY = () => {
+  let newArray = [];
+  for (let i = 0; i < 10; i++) {
+    newArray.push(GET_OBJECT());
+  }
+  return newArray;
+};
+
+const CHECK = CREATE_ARRAY();
