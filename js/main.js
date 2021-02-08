9da0cd128245baa86824b789076b5da9d48af258
Zvereck20 <kerusov20@gmail.com>
2021-02-08 19:31:32 +1000

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-unused-vars */
 // Функция возвращает целое число
 
-// eslint-disable-next-line no-unused-vars
-const RETURN_INTEGER = (minNumber, maxNumber) => {
+let returnRandomnInteger = (minNumber, maxNumber) => {
   if (minNumber >= maxNumber) {
     return 'Не верно введенные числа'
   }
@@ -11,13 +11,82 @@ const RETURN_INTEGER = (minNumber, maxNumber) => {
 
 // Функция, возвращающая случайное число с плавающей точкой
 
-// eslint-disable-next-line no-unused-vars
-const RETURN_FRACTIONAL = (minNumber, maxNumber, anotherNumber = 1) => {
-  let randomNumber;
+let returnRandomFractional = (minNumber, maxNumber, anotherNumber = 1) => {
   if (minNumber >= maxNumber) {
     return 'Не верно введенные числа'
   }
-  // eslint-disable-next-line no-unused-vars
-  randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+  let randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
   return randomNumber.toFixed(anotherNumber);
+};
+
+
+// Authur object
+
+let authorObject = {
+  avatar: () => {
+    let randomAdress = '0' + Math.round(Math.random() * (8 - 1) + 1);
+    return 'img/avatars/user' + randomAdress + '.png';
+  },
+};
+
+// Offer object
+
+let offerObject = {
+  title: 'Предложения',
+  address: '{{location.x}, {location.y}}',
+  price: 2800,
+  type: 'bungallow',
+  rooms: 2,
+  quests: 4,
+  checkin: '12:00',
+  checkout: '13:00',
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  description: 'Вид на море',
+  photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+};
+
+// Location object
+
+let locationObject = {
+  randomLatitude: () => {
+    let latitudeNumber = Math.random() * (35.70000 - 35.65000) + 35.65000;
+    return latitudeNumber.toFixed(5);
+  },
+  randomLongitude: () => {
+    let longitubeNumber = Math.random() * (139.80000 - 139.70000) + 139.70000;
+    return longitubeNumber.toFixed(5);
+  },
+};
+
+//  Generated object
+let generatedObject = {
+  authorObject: {
+    avatar: () => {
+      let randomAdress = '0' + Math.round(Math.random() * (8 - 1) + 1);
+      return 'img/avatars/user' + randomAdress + '.png';
+    },
+  },
+  offerObject: {
+    title: 'Предложения',
+    address: '{{location.x}, {location.y}}',
+    price: 2800,
+    type: 'bungallow',
+    rooms: 2,
+    quests: 4,
+    checkin: '12:00',
+    checkout: '13:00',
+    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+    description: 'Вид на море',
+    photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+  },
+  locationObject: {
+    randomLatitude: () => {
+      let latitudeNumber = Math.random() * (35.70000 - 35.65000) + 35.65000;
+      return latitudeNumber.toFixed(5);
+    },
+    randomLongitude: () => {
+      let longitubeNumber = Math.random() * (139.80000 - 139.70000) + 139.70000;
+      return longitubeNumber.toFixed(5);
+    },
+  },
 };
